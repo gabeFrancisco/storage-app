68a2f6c9f8c75ae3b25dfca9ab90875ddb95825c
gabeFrancisco <gabegit2@gmail.com>
2025-03-20 22:06:43 -0300

    created Category model and added relations with Product | made migrations

--- database/migrations/2025_03_21_003428_create_categories_table.php
+++ database/migrations/2025_03_21_003428_create_categories_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table){
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('name');
             $table->string('description');
-            $table->integer('quantity');
-            $table->double('price');
         });
     }
 
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 };
--- database/migrations/2025_03_21_010525_product_props.php
+++ database/migrations/2025_03_21_010525_product_props.php
@@ -11,9 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table){
-            $table->id();
-            $table->timestamps();
+        Schema::table("products", function (Blueprint $table) {
             $table->string('name');
             $table->string('description');
             $table->integer('quantity');
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        //
     }
 };
